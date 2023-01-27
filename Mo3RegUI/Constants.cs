092a5723cad22141fa72d668898b11377860ae4f
Sad Pencil <sadpencil@outlook.com>
2023-01-27 23:07:20 +0800

    Bump version to v2.1.0

--- Mo3RegUI/Constants.cs
+++ Mo3RegUI/Constants.cs
@@ -4,7 +4,7 @@
     {
         public static readonly string Name = "Mental Omega 3.3.6 注册机";
         public static readonly string Author = "伤心的笔";
-        public static readonly string Version = "v2.0.0";
+        public static readonly string Version = "v2.1.0";
         public static readonly string RepoUri = "https://github.com/SadPencil/Mo3RegUI";
     }
 }
