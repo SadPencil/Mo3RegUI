ca39a570c6b7a0f3cf72b23e820f20c214448ba6
Freeze AutoGavy <1021093715@qq.com>
2025-02-02 14:28:09 +0800

    update

---
+++ Mo3RegUI/NativeConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Mo3RegUI
+{
+    public class NativeConstants
+    {
+        /// SM_CXSCREEN -> 0
+        public const int SM_CXSCREEN = 0;
+
+        /// SM_CYSCREEN -> 1
+        public const int SM_CYSCREEN = 1;
+    }
+}
